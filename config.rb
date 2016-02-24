fa42ff9c01b1130482936de51e6d49a1283fe343
KevinAdu <kevyugi@gmail.com>
2016-02-24 11:43:27 +0900

    Added ruby script to remove comments while compiling with compass

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require './remove-all-comments'
 add_import_path "bower_components/foundation-sites/scss"
 
 # Set this to the root of your project when deployed:
