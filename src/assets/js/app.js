6821864215a132b3c521d4b662e13244428fbdc7
KevinAdu <kevyugi@gmail.com>
2016-03-04 16:30:05 +0900

    Day picker can now cycle through weeks

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,26 +1,36 @@
 (function() {
-	$('.day-picker-frame').sly({
-		horizontal: true,
-		itemNav: 'basic',
-		smart: true,
-		activateOn: 'click',
-		mouseDragging: true,
-		touchDragging: true,
-		releaseSwing: true,
-		activatePageOn: 'click',
-		speed: 300,
-		elasticBounds: true,
-		dragHandle: true,
-		dynamicHandle: true,
-		clickBar: true,
-		startAt: $('.day-picker-frame li').index($('.day-picker-frame li.active')),
-		prev: "button.previous-week",
-		next: "button.next-week"
-	}).init();
+	var $frame = $('.day-picker-frame');
+  var $wrap  = $frame.parent();
 
-	$(window).resize(function(e) {
-		$('.day-picker-frame').sly('reload');
-	});
+  // Call Sly on frame
+  $frame.sly({
+    horizontal: 1,
+    itemNav: 'centered',
+    smart: 1,
+    activateOn: 'click',
+    mouseDragging: 1,
+    touchDragging: 1,
+    releaseSwing: 1,
+    pagesBar: $wrap.find('.pages'),
+    activatePageOn: 'click',
+    speed: 300,
+    elasticBounds: 1,
+    dragHandle: 1,
+    dynamicHandle: 1,
+    clickBar: 1,
+
+    startAt: $('.day-picker-frame li').index($('.day-picker-frame li.active')),
+
+    // Cycling
+    cycleBy: 'pages',
+    cycleInterval: 1000,
+    pauseOnHover: 1,
+    startPaused: 1,
+
+    // Buttons
+    prevPage: $('button.previous-week'),
+    nextPage: $('button.next-week')
+  });
 
 	$(".show-feedback").click(function() {
 		$(this).parent().siblings().find(".assessment-feedback").toggle();
