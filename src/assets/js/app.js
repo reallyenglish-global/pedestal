5f106d534f4439b661457d7dd36e29370eceda65
KevinAdu <kevyugi@gmail.com>
2016-03-18 12:46:54 +0900

    Added responsive styles to the study tasks page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,6 @@
 (function() {
-	var $frame = $('.day-picker-frame');
+  $(document).foundation();
+  var $frame = $('.day-picker-frame');
   var $wrap  = $frame.parent();
 
   // Call Sly on frame
@@ -32,28 +33,27 @@
     nextPage: $('button.next-week')
   });
 
-	$(".show-feedback").click(function() {
-		$(this).parent().siblings().find(".assessment-feedback").toggle();
-	});
+  $(".show-feedback").click(function() {
+    $(this).parent().siblings().find(".assessment-feedback").toggle();
+  });
 
-    $(".show-answer").click(function() {
-        $(this).closest(".row").siblings(".faq-answer").toggle();
-        $(this).toggleClass(function() {
-            if($(this).hasClass("show-answer")) {
-                return "hide-answer";
-            } else {
-                return "show-answer";
-            }
-        });
+  $(".show-answer").click(function() {
+    $(this).closest(".row").siblings(".faq-answer").toggle();
+    $(this).toggleClass(function() {
+      if($(this).hasClass("show-answer")) {
+        return "hide-answer";
+      } else {
+        return "show-answer";
+      }
     });
+  });
 
-    var toggleBookings = function() {
+  var toggleBookings = function() {
+    $(".early-bookings").toggleClass("hide");
+    $(".show-bookings").toggleClass("hide");
+    $(".hide-bookings").toggleClass("hide");
+  };
 
-        $(".early-bookings").toggleClass("hide");
-        $(".show-bookings").toggleClass("hide");
-        $(".hide-bookings").toggleClass("hide");
-    };
-
-    $(".show-bookings").click(toggleBookings);
-    $(".hide-bookings").click(toggleBookings);
+  $(".show-bookings").click(toggleBookings);
+  $(".hide-bookings").click(toggleBookings);
 })();
